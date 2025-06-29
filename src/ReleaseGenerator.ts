6263efc79a88ac5bf4c47347d516b2cfcf508475
Alex Chekulaev <chekulaevalexey@gmail.com>
2025-06-29 19:44:52 -0400

    fix issue with semantic-release loading

--- src/ReleaseGenerator.ts
+++ src/ReleaseGenerator.ts
@@ -1,16 +1,29 @@
-import fs from 'fs'
-import semanticRelease, { type NextRelease, type Options, type Result } from 'semantic-release'
+// const semanticRelease = require('semantic-release')
+// import fs from 'fs'
+// import {default as semanticRelease} from 'semantic-release'
+// import { type Options } from 'semantic-release'
+// import semanticRelease, { type NextRelease, type Options, type Result } from 'semantic-release'
 
 export class ReleaseGenerator {
   async generate(branch: string) {
-    const options: Options = {
+    const semanticRelease = await import('semantic-release')
+
+    const res = await semanticRelease.default({
       branches: [branch],
-      plugins: [
-        '@semantic-release/commit-analyzer',
-        '@semantic-release/release-notes-generator',
-      ],
-      dryRun: true,
-    }
+      dryRun: true
+    })
+
+    console.log(res)
+
+    // const options: Options = {
+    //   branches: [branch],
+    //   plugins: [
+    //     '@semantic-release/commit-analyzer',
+    //     '@semantic-release/release-notes-generator',
+    //   ],
+    //   dryRun: true,
+    // }
+    // console.log(options)
 
     // const environment: Config = {
     //   cwd: process.cwd(),
@@ -19,14 +32,14 @@ export class ReleaseGenerator {
     //   stderr: process.stderr,
     // };
 
-    const result: Result = await semanticRelease(options)// , environment);
-    if (!result) {
-      throw 'Semantic Release did not return a result.'
-    }
-    const nextRelease: NextRelease = result.nextRelease
-    fs.writeFileSync('next_version', nextRelease.version)
-    if (nextRelease.notes) {
-      fs.writeFileSync('release_notes', nextRelease.notes)
-    }
+    // const result: Result = await semanticRelease(options)// , environment);
+    // if (!result) {
+    //   throw 'Semantic Release did not return a result.'
+    // }
+    // const nextRelease: NextRelease = result.nextRelease
+    // fs.writeFileSync('next_version', nextRelease.version)
+    // if (nextRelease.notes) {
+    //   fs.writeFileSync('release_notes', nextRelease.notes)
+    // }
   }
 }
