00570c3e5b1f0a5519ef44524d0abf16cd4acea6
Alex Chekulaev <chekulaevalexey@gmail.com>
2025-07-02 09:05:37 -0400

    fix: index.ts work with dynamic imports

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,19 @@
 import { exec } from 'node:child_process'
 import * as path from 'node:path'
 import * as util from 'node:util'
-import * as core from '@actions/core'
 
-const run = async () => {
-  // Install Dependencies
-  if (process.env.CI) {
-    const execAsync = util.promisify(exec)
-    const { stdout, stderr } = await execAsync('npm --loglevel error ci --only=prod', {
-      cwd: path.resolve(__dirname)
-    })
-    console.log(stdout)
-    if (stderr) {
-      return Promise.reject(stderr)
-    }
+if (process.env.CI) {
+  const execAsync = util.promisify(exec)
+  const { stdout, stderr } = await execAsync('npm --loglevel error ci --only=prod', {
+    cwd: path.resolve(__dirname)
+  })
+  console.log(stdout)
+  if (stderr) {
+    console.error(stderr)
+    process.exit(1);
   }
-
-  const { release } = await import('./release.js')
-  await release()
 }
 
-run().catch(core.setFailed)
+const core = await import('@actions/core');
+const { release } = await import('./release.js')
+await release().catch(core.setFailed)
