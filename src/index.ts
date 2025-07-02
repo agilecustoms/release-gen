2306ecbd807a90d5dc0b00caee579671208b0a35
Alex Chekulaev <chekulaevalexey@gmail.com>
2025-07-02 09:28:59 -0400

    fix: npm packages resolution

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
 import { exec } from 'node:child_process'
 import * as path from 'node:path'
+import { fileURLToPath } from 'node:url'
 import * as util from 'node:util'
 
-if (process.env.CI) {
-  const execAsync = util.promisify(exec)
-  const { stdout, stderr } = await execAsync('npm --loglevel error ci --only=prod', {
-    cwd: path.resolve(__dirname)
-  })
-  console.log(stdout)
-  if (stderr) {
-    console.error(stderr)
-    process.exit(1)
-  }
+const distDir = path.dirname(fileURLToPath(import.meta.url))
+const packageJsonDir = path.dirname(distDir)
+const execAsync = util.promisify(exec)
+const { stdout, stderr } = await execAsync('npm --loglevel error ci --only=prod', {
+  cwd: packageJsonDir
+})
+console.log(stdout)
+if (stderr) {
+  console.error('Error during npm ci - packages installed dynamically at runtime')
+  console.error(stderr)
+  process.exit(1)
 }
 
 const core = await import('@actions/core')
