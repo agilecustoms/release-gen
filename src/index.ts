56ac0a2a4c74be98790e8645aaa78bb438c752b9
Alex Chekulaev <chekulaevalexey@gmail.com>
2025-07-15 09:20:36 -0400

    test commit2

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ let { stdout, stderr } = await execAsync('npm --loglevel error ci --only=prod', 
 console.log(stdout)
 if (stderr) {
   console.error('Error during npm ci - packages installed dynamically at runtime')
-  console.error(stderr)
+  console.error(stderr);
   process.exit(1);
 }
 
