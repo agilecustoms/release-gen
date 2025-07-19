f21e6fdfbb2ac6373bac1977a1905bd9d8f0b3cf
Alex Chekulaev <chekulaevalexey@gmail.com>
2025-07-19 14:14:48 -0400

    improve eslint config [skip ci]

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,27 @@
-import tseslint from 'typescript-eslint'
 import plugin from '@stylistic/eslint-plugin'
-import importPlugin from 'eslint-plugin-import';
+import importPlugin from 'eslint-plugin-import'
+import tseslint from 'typescript-eslint'
 
 export default [
-    ...tseslint.configs.recommended,
-    plugin.configs['recommended'],
-    {
-        plugins: {
-            import: importPlugin,
-        },
-        rules: {
-            '@stylistic/brace-style': ['error', '1tbs'], // 'else' keyword on the same line as closing brace
-            '@stylistic/comma-dangle': 'off', // there are cases when trailing comma desired, and sometimes not
-            '@typescript-eslint/no-unused-vars': ['error', { // unused var starting from _ is ok
-                'argsIgnorePattern': '^_',
-                'destructuredArrayIgnorePattern': '^_'
-            }],
+  ...tseslint.configs.recommended,
+  plugin.configs['recommended'],
+  {
+    plugins: {
+      import: importPlugin,
+    },
+    rules: {
+      '@stylistic/brace-style': ['error', '1tbs'], // 'else' keyword on the same line as closing brace
+      '@stylistic/comma-dangle': 'off', // there are cases when trailing comma desired, and sometimes not
+      '@typescript-eslint/no-unused-vars': ['error', { // unused var starting from _ is ok
+        argsIgnorePattern: '^_',
+        destructuredArrayIgnorePattern: '^_'
+      }],
 
-            // Enforce Alphabetical Import Order and Merge Duplicate Imports
-            'import/order': ['error', {
-                'alphabetize': {'order': 'asc', 'caseInsensitive': true}
-            }],
-            'import/no-duplicates': 'error',
-        }
+      // Enforce Alphabetical Import Order and Merge Duplicate Imports
+      'import/order': ['error', {
+        alphabetize: { order: 'asc', caseInsensitive: true }
+      }],
+      'import/no-duplicates': 'error',
     }
-];
+  }
+]
